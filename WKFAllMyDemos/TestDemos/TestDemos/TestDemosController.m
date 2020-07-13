3a758750e05adf3dd3b71af45e7742fb1749b000
wangkongfei <wangkongfei@skrer.net>
2020-07-13 09:15:06 +0800

    add test vc

--- WKFAllMyDemos/TestDemos/TestDemos/TestDemosController.m
+++ WKFAllMyDemos/TestDemos/TestDemos/TestDemosController.m
@@ -37,75 +37,87 @@
     
     [super viewDidLoad];
     
-    [self setupViews];
+    NSMutableArray *array = [NSMutableArray array];
+    [array addObject:@"1"];
+    [array addObject:@"2"];
+    [array addObject:@"3"];
+    NSLog(@"111array == %p", array);
+    id obj = array.firstObject;
+    NSLog(@"111obj == %p", obj);
+    
+    [array removeObjectAtIndex:0];
+    NSLog(@"array222 == %p", array);
+    id obj1 = array.firstObject;
+    NSLog(@"obj222 == %p", obj1);
+//    [self setupViews];
     
 }
 - (void)dealloc{
-   TCBlobDownloadManager * shareManager = [TCBlobDownloadManager sharedInstance];
+    TCBlobDownloadManager *shareManager = [TCBlobDownloadManager sharedInstance];
     [shareManager cancelAllDownloadsAndRemoveFiles:NO];
 }
 
 
 - (void)clickDownloadButton1:(UIButton*)button{
-    NSString * kCachePath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
-    NSString * path = [kCachePath stringByAppendingString:@"/Videos/111.mp4"];
-    NSLog(@"kCachePath ==%@",kCachePath);
-    NSLog(@"path ==%@",path);
-    [[FGGDownloadManager shredManager] downloadWithUrlString:urlString toPath:path process:^(float progress, NSString *sizeString, NSString *speedString) {
-        NSLog(@"progress ==%f",progress);
-    } completion:^{
-        NSLog(@"completion");
-    } failure:^(NSError *error) {
-        NSLog(@"error ==%@",error.localizedDescription);
-    }];
-    
-//    UIStatusBar * bar = [[UIStatusBar alloc]init];
-    
-//    FGGDownloadManager * downloadManger = [FGGDownloadManager shredManager];
-//    [downloadManger downloadWithUrlString:urlString toPath:nil process:^(float progress, NSString *sizeString, NSString *speedString) {
-//        NSLog(@"progress ==%f sizeString==%@ speedString==%@",progress,sizeString,speedString);
+//    NSString * kCachePath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+//    NSString * path = [kCachePath stringByAppendingString:@"/Videos/111.mp4"];
+//    NSLog(@"kCachePath ==%@",kCachePath);
+//    NSLog(@"path ==%@",path);
+//    [[FGGDownloadManager shredManager] downloadWithUrlString:urlString toPath:path process:^(float progress, NSString *sizeString, NSString *speedString) {
+//        NSLog(@"progress ==%f",progress);
 //    } completion:^{
 //        NSLog(@"completion");
 //    } failure:^(NSError *error) {
 //        NSLog(@"error ==%@",error.localizedDescription);
 //    }];
     
-//    TCBlobDownloadManager * shareManager = [TCBlobDownloadManager sharedInstance];
-//
-//    NSURL * url = [NSURL URLWithString:urlString];
-//
-//    TCBlobDownloader * downloader = [shareManager startDownloadWithURL:url customPath:nil firstResponse:^(NSURLResponse *response) {
-//
-//        NSLog(@"firstResponse response==%@",response);
-//
-//    } progress:^(uint64_t receivedLength, uint64_t totalLength, NSInteger remainingTime, float progress) {
-//        NSLog(@"remainingTime ==%ld",(long)remainingTime);
-//        NSLog(@"progress ==%f",progress);
-//        NSLog(@"totalLength ==%f",totalLength);
-//
-//    } error:^(NSError *error) {
-//        NSLog(@"error ==%@",error.localizedDescription);
-//
-//    } complete:^(BOOL downloadFinished, NSString *pathToFile) {
-//        NSLog(@"downloadFinished ==%d",downloadFinished);
-//        NSLog(@"pathToFile ==%@",pathToFile);
-//    }];
+    //    UIStatusBar * bar = [[UIStatusBar alloc]init];
+    
+    //    FGGDownloadManager * downloadManger = [FGGDownloadManager shredManager];
+    //    [downloadManger downloadWithUrlString:urlString toPath:nil process:^(float progress, NSString *sizeString, NSString *speedString) {
+    //        NSLog(@"progress ==%f sizeString==%@ speedString==%@",progress,sizeString,speedString);
+    //    } completion:^{
+    //        NSLog(@"completion");
+    //    } failure:^(NSError *error) {
+    //        NSLog(@"error ==%@",error.localizedDescription);
+    //    }];
+    
+    //    TCBlobDownloadManager * shareManager = [TCBlobDownloadManager sharedInstance];
+    //
+    //    NSURL * url = [NSURL URLWithString:urlString];
+    //
+    //    TCBlobDownloader * downloader = [shareManager startDownloadWithURL:url customPath:nil firstResponse:^(NSURLResponse *response) {
+    //
+    //        NSLog(@"firstResponse response==%@",response);
+    //
+    //    } progress:^(uint64_t receivedLength, uint64_t totalLength, NSInteger remainingTime, float progress) {
+    //        NSLog(@"remainingTime ==%ld",(long)remainingTime);
+    //        NSLog(@"progress ==%f",progress);
+    //        NSLog(@"totalLength ==%f",totalLength);
+    //
+    //    } error:^(NSError *error) {
+    //        NSLog(@"error ==%@",error.localizedDescription);
+    //
+    //    } complete:^(BOOL downloadFinished, NSString *pathToFile) {
+    //        NSLog(@"downloadFinished ==%d",downloadFinished);
+    //        NSLog(@"pathToFile ==%@",pathToFile);
+    //    }];
     
 }
 
-- (void)setupViews{
-    UIButton * button1 = [UIButton buttonWithType:UIButtonTypeCustom];
-    button1.backgroundColor = [UIColor blueColor];
-    [button1 addTarget:self action:@selector(clickDownloadButton1:) forControlEvents:UIControlEventTouchUpInside];
-    [button1 setTitle:@"download1" forState:UIControlStateNormal];
-    [self.view addSubview:button1];
-    [button1 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(20);
-        make.top.mas_equalTo(64+20);
-        make.width.mas_equalTo(120);
-        make.height.mas_equalTo(40);
-    }];
-}
+//- (void)setupViews{
+//    UIButton * button1 = [UIButton buttonWithType:UIButtonTypeCustom];
+//    button1.backgroundColor = [UIColor blueColor];
+//    [button1 addTarget:self action:@selector(clickDownloadButton1:) forControlEvents:UIControlEventTouchUpInside];
+//    [button1 setTitle:@"download1" forState:UIControlStateNormal];
+//    [self.view addSubview:button1];
+//    [button1 mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.left.mas_equalTo(20);
+//        make.top.mas_equalTo(64+20);
+//        make.width.mas_equalTo(120);
+//        make.height.mas_equalTo(40);
+//    }];
+//}
 
 
 @end
